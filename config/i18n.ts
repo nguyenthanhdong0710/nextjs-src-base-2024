b1e6549fe6c41c09a90f780c9ab9562268906099
dongnt <nguyenthanhdong0710@gmail.com>
2024-08-21 02:26:38 +0700

    test

--- config/i18n.ts
+++ config/i18n.ts
@@ -1,12 +1,7 @@
 import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
 
-export const supportedLanguages = ["en", "vi"
-  
-
-
-  
-];
+export const supportedLanguages = ["en", "vi"];
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
